ce835695f0bf878d3a97071058f93b490bade64d
triaz0812 <talat.riaz@gmail.com>
2025-07-04 18:58:02 -0400

    flask deployment

---
+++ server.py
@@ -0,0 +1,28 @@
+''' Executing this function initiates the application of sentiment
+    analysis to be executed over the Flask channel and deployed on
+    localhost:5000.
+'''
+from flask import Flask, render_template, request
+from EmotionDetection.emotion_detection import emotion_detector
+
+#initiate flask app
+app = Flask("Emotion Detector")
+@app.route("/emotionDetector")
+def emot_detector():
+    '''function retrieves the text input and checks for errors'''
+    # Retrieve text from request arguments
+    text_to_analyze = request.args.get('textToAnalyze')
+    # Pass text to emotion detector function and store response
+    response = emotion_detector(text_to_analyze)
+    # Output response
+    return response
+
+@app.route("/")
+def render_index_page():
+    ''' This function initiates the rendering of the main application
+        page over the Flask channel'''
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    # This functions executes the flask app and deploys it on localhost:5000
+    app.run(host="0.0.0.0", port=5000)
