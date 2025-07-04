a4932c41521b1a785ca25243ae61257478dd6a1f
triaz0812 <talat.riaz@gmail.com>
2025-07-04 14:27:10 -0400

    updated s3 exercise

--- emotion_detection.py
+++ emotion_detection.py
@@ -17,5 +17,19 @@ def emotion_detector(text_to_analyse):
     #Parse API response
     formatted_response = json.loads(response.text)
 
+    #Get top-level emotion name value pairs and populate extracted dict
+    extracted = {}
+   '''Use get() method to avoid error or crashes. If no vaue returns default specified, i.e first instance list with empty dict and in second instance an empty dict'''
+
+    emotions = formatted_response.get('emotionPredictions', [{}])[0].get('emotion', {})
+    extracted.update(emotions)
+
+    #Calculate dominant emotion
+    '''Use max() method with key to find highest score. Avoids using manual loop and more efficient. Could use an if statement to check extracted is not empty for more stability. May add later'''
+
+    dominant_emotion = max(extracted, key=extracted.get)
+    extracted['dominant_emotion'] = dominant_emotion
+
     #Return result
-    return formatted_response
+    return extracted
+   
