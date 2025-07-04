702d699282d8624454538960e1614cf74f764d11
triaz0812 <talat.riaz@gmail.com>
2025-07-04 19:18:33 -0400

    static code analysis first pass

--- server.py
+++ server.py
@@ -36,7 +36,7 @@ def render_index_page():
     ''' This function initiates the rendering of the main application
         page over the Flask channel'''
     return render_template('index.html')
-    
+
 if __name__ == "__main__":
     # This functions executes the flask app and deploys it on localhost:5000
     app.run(host="0.0.0.0", port=5000)
